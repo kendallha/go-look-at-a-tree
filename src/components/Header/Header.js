ce62682c3d311b5639c3e8a9da09544269e0b2fd
kendallha <kendallhaw@gmail.com>
2021-05-29 11:12:25 -0700

    Add JSX in Header component return statement

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Header = () => {
   return (
-
+    <header>
+      <h1>Go Look at a Tree</h1>
+      <h2>Get a Tree</h2>
+      <h2>Add a Tree</h2>
+    </header>
   )
 }
 
