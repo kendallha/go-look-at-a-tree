583a37d67cfb897e7660f198b31857fb36d1b6b5
kendallha <kendallhaw@gmail.com>
2021-05-29 11:56:42 -0700

    Add JSX for form, inputs, and button in render of Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,6 @@ class Form extends Component {
   constructor() {
     super()
     this.state = {
-      "id": '',
       "name": '',
       "region": '',
       "scientificName": '',
@@ -13,5 +12,20 @@ class Form extends Component {
       "fact": '',
       "image": ''
     }
+  }
+
+  render() {
+    return(
+      <form>
+        <input type='text' placeholder='Common name' name='name' value={this.state.name}/>
+        <input type='text' placeholder='Scientific name' name='scientificName' value={this.state.scientificName}/>
+        <input type='text' placeholder='Region' name='region' value={this.state.region}/>
+        <input type='text' placeholder='Average height' name='averageHeight' value={this.state.averageHeight}/>
+        <input type='text' placeholder='Lifespan' name='lifespan' value={this.state.lifespan}/>
+        <input type='text' placeholder='Fun fact' name='fact' value={this.state.fact}/>
+        <input type='text' placeholder='Image URL' name='image' value={this.state.image}/>
+        <button>Add Tree</button>
+      </form>
+    )
   }
 }
