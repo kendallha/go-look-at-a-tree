fcc9dbce135a8aa19de561bd260477753a4c389b
kendallha <kendallhaw@gmail.com>
2021-05-29 10:18:29 -0700

    Add additional key-value pairs to mock data to build out complete TreeDisplay component

--- src/assets/treeData.js
+++ src/assets/treeData.js
@@ -1,5 +1,10 @@
 const treeData = [
-    { "type": "oak" }
+    { "id": 1,
+      "name": "Quaking Aspen",
+      "scientific_name": "Populus tremuloides",
+      "average_height": "20-80 feet",
+      "lifespan": "50-60 years",
+      "fact": "A grove of quaking aspens in Utah is the largest known living thing on Earth. Nearly 50,000 stems protrude from a single root system. The entire organism covers over 100 acres and weighs 6,000 tons." }
   ]
 
 export default treeData
