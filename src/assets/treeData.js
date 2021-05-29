6b9f32d93cc42b3bb3183fb72e2ca49b737eb2d3
kendallha <kendallhaw@gmail.com>
2021-05-29 10:20:27 -0700

    Create functional component for TreeDisplay

--- src/assets/treeData.js
+++ src/assets/treeData.js
@@ -1,8 +1,8 @@
 const treeData = [
     { "id": 1,
       "name": "Quaking Aspen",
-      "scientific_name": "Populus tremuloides",
-      "average_height": "20-80 feet",
+      "scientificName": "Populus tremuloides",
+      "averageHeight": "20-80 feet",
       "lifespan": "50-60 years",
       "fact": "A grove of quaking aspens in Utah is the largest known living thing on Earth. Nearly 50,000 stems protrude from a single root system. The entire organism covers over 100 acres and weighs 6,000 tons." }
   ]
---
+++ src/components/TreeDisplay/TreeDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TreeDisplay = () => {
+  return (
+
+  )
+}
+
+export default TreeDisplay;
