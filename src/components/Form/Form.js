ad73e9220c4653354ec21fd9f6ee0ccaa4bc8b10
kendallha <kendallhaw@gmail.com>
2021-05-29 11:51:41 -0700

    Add class component for Form and state for eventual inputs

---
+++ src/components/Form/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      "id": '',
+      "name": '',
+      "region": '',
+      "scientificName": '',
+      "averageHeight": '',
+      "lifespan": '',
+      "fact": '',
+      "image": ''
+    }
+  }
+}
