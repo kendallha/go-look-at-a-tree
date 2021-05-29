78c900b1b08d1c282ea500c1a94032da5e744b59
kendallha <kendallhaw@gmail.com>
2021-05-29 10:30:55 -0700

    Add return statement to TreeDisplay with JSX using props

--- src/components/TreeDisplay/TreeDisplay.js
+++ src/components/TreeDisplay/TreeDisplay.js
@@ -3,7 +3,12 @@ import React from 'react';
 const TreeDisplay = ({ tree }) => {
   return (
     <article>
-      <img src={imgURL}
+      <img src={tree.image} />
+      <h1>{tree.name}</h1>
+      <h2>{tree.scientificName}</h2>
+      <p>{tree.averageHeight}</p>
+      <p>{tree.lifespan}</p>
+      <p>{tree.fact}</p>
     </article>
   )
 }
