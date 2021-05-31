b834cf568eef6ec0046fd8ed01237c405bec1c3d
Zach Johnson <zachjjohns@gmail.com>
2021-05-30 19:17:19 -0500

    Rearrange TreeDisplay containers, apply tree-image styling

--- src/components/TreeDisplay/TreeDisplay.js
+++ src/components/TreeDisplay/TreeDisplay.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './TreeDisplay.css';
 
 const TreeDisplay = ({ tree }) => {
   return (
     <article>
       <img alt={`${tree.name} tree`} src={tree.image} className="tree-image"/>
-      <h1 className="tree-name">{tree.name}</h1>
-      <h2 className="sci-name">{tree.scientific_name}</h2>
-      <p className="region">Region: {tree.region}</p>
-      <p className="height">Average height: {tree.average_height} feet</p>
-      <p className="lifespan">Lifespan: {tree.lifespan} years</p>
-      <p className="tree-fact">{tree.fact}</p>
+      <div className="tree-details">
+        <h1 className="tree-name">{tree.name}</h1>
+        <h2 className="sci-name">{tree.scientific_name}</h2>
+        <p className="region">Region: {tree.region}</p>
+        <p className="height">Average height: {tree.average_height} feet</p>
+        <p className="lifespan">Lifespan: {tree.lifespan} years</p>
+        <p className="tree-fact">{tree.fact}</p>
+      </div>
     </article>
   )
 }
