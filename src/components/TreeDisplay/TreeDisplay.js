65130630e087bee4a2c862f461d197d7bf0776d7
Zach Johnson <zachjjohns@gmail.com>
2021-05-30 19:07:40 -0500

    Add classNames to TreeDisplay JSX

--- src/components/TreeDisplay/TreeDisplay.js
+++ src/components/TreeDisplay/TreeDisplay.js
@@ -3,13 +3,13 @@ import React from 'react';
 const TreeDisplay = ({ tree }) => {
   return (
     <article>
-      <img alt={`${tree.name} tree`} src={tree.image} />
-      <h1>{tree.name}</h1>
-      <h2>{tree.scientific_name}</h2>
-      <p>Region: {tree.region}</p>
-      <p>Average height: {tree.average_height} feet</p>
-      <p>Lifespan: {tree.lifespan} years</p>
-      <p>{tree.fact}</p>
+      <img alt={`${tree.name} tree`} src={tree.image} className="tree-image"/>
+      <h1 className="tree-name">{tree.name}</h1>
+      <h2 className="sci-name">{tree.scientific_name}</h2>
+      <p className="region">Region: {tree.region}</p>
+      <p className="height">Average height: {tree.average_height} feet</p>
+      <p className="lifespan">Lifespan: {tree.lifespan} years</p>
+      <p className="tree-fact">{tree.fact}</p>
     </article>
   )
 }
