c1cc3ce1692a368368bbfe6bcad64a4450f65d0a
Riley Martin <riley.willow@gmail.com>
2021-06-05 08:55:24 -0600

    Add intercepts to cypress tests

--- cypress/integration/TreeDisplay_spec.js
+++ cypress/integration/TreeDisplay_spec.js
@@ -1,16 +1,26 @@
 describe('TreeDisplay', () => {
   beforeEach(() => {
-    //intercept and give it only one tree
-    // { id: "1",
-    //   name: "Quaking Aspen",
-    //   region: "Cooler areas of North America",
-    //   scientific_name: "Populus tremuloides",
-    //   average_height: "20-80",
-    //   lifespan: "50-60",
-    //   fact: "A grove of quaking aspens in Utah is the largest known living thing on Earth. Nearly 50,000 stems protrude from a single root system. The entire organism covers over 100 acres and weighs 6,000 tons.",
-    //   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw4wazSVis80ntW-mjkHu-_KwiUsBJ2OwFwtP4A1pOfR8zmrl1-FvVJcLBXDsxBAmoqP0&usqp=CAU"
-    // }
-    cy.visit('http://localhost:3000')
+    cy.intercept('https://go-look-at-a-tree-api.herokuapp.com/api/v1/trees', [
+        { id: "1",
+          name: "Quaking Aspen",
+          region: "Cooler areas of North America",
+          scientific_name: "Populus tremuloides",
+          average_height: "20-80",
+          lifespan: "50-60",
+          fact: "A grove of quaking aspens in Utah is the largest known living thing on Earth. Nearly 50,000 stems protrude from a single root system. The entire organism covers over 100 acres and weighs 6,000 tons.",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw4wazSVis80ntW-mjkHu-_KwiUsBJ2OwFwtP4A1pOfR8zmrl1-FvVJcLBXDsxBAmoqP0&usqp=CAU"
+        },
+        { id: "2",
+          name: "A Fine Oak",
+          region: "Everywhere",
+          scientific_name: "Oakus maximus",
+          average_height: "20-80",
+          lifespan: "50-60",
+          fact: "A grove of quaking aspens in Utah is the largest known living thing on Earth. Nearly 50,000 stems protrude from a single root system. The entire organism covers over 100 acres and weighs 6,000 tons.",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw4wazSVis80ntW-mjkHu-_KwiUsBJ2OwFwtP4A1pOfR8zmrl1-FvVJcLBXDsxBAmoqP0&usqp=CAU"
+        }
+      ])
+      .visit('http://localhost:3000')
   })
   it('Should display a random tree name', () => {
     cy.get('.tree-name').should('have.text', 'Quaking Aspen')
