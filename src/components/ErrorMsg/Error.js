6b51e2da1dda6d7b68fc990e0c083fd4a1b8a53a
Riley Martin <riley.willow@gmail.com>
2021-06-05 20:00:09 -0600

    Create error component for error handling

---
+++ src/components/ErrorMsg/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Error.css';
+
+const Error = ({ error }) => {
+  return (
+    <>
+      <h1>Error: {error}</h1>
+    </>
+  )
+}
+
+export default Error;
--- src/components/TreeDisplay/TreeDisplay.js
+++ src/components/TreeDisplay/TreeDisplay.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './TreeDisplay.css';
+import Error from '../ErrorMsg/Error'
 
 const TreeDisplay = ({ tree, error }) => {
   return (
@@ -7,17 +8,22 @@ const TreeDisplay = ({ tree, error }) => {
       {!tree &&
         <h1>Loading...</h1>
       }
-      <article className='tree-display'>
-        <img alt={`${tree.name} tree`} src={tree.image} className="tree-image"/>
-        <div className="tree-details">
-          <h1 className="tree-name">{tree.name}</h1>
-          <h2 className="sci-name">{tree.scientific_name}</h2>
-          <p className="region">Region: {tree.region}</p>
-          <p className="height">Average height: {tree.average_height} feet</p>
-          <p className="lifespan">Lifespan: {tree.lifespan} years</p>
-          <p className="tree-fact">{tree.fact}</p>
-        </div>
-      </article>
+      {error &&
+        <Error error={error}/>
+      }
+      {!error && tree &&
+        <article className='tree-display'>
+          <img alt={`${tree.name} tree`} src={tree.image} className="tree-image"/>
+          <div className="tree-details">
+            <h1 className="tree-name">{tree.name}</h1>
+            <h2 className="sci-name">{tree.scientific_name}</h2>
+            <p className="region">Region: {tree.region}</p>
+            <p className="height">Average height: {tree.average_height} feet</p>
+            <p className="lifespan">Lifespan: {tree.lifespan} years</p>
+            <p className="tree-fact">{tree.fact}</p>
+          </div>
+        </article>
+      }
     </>
   )
 }
