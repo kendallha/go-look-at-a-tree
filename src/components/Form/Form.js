e9951c7631ceff0371a129ab50af283aeba08006
Zach Johnson <zachjjohns@gmail.com>
2021-06-05 20:12:09 -0500

    Apply form-row containers for each input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -44,62 +44,76 @@ class Form extends Component {
     return(
       <form>
         <h1>Add Your Favorite Tree</h1>
-        <label>Common name</label>
-        <input
-          type='text'
-          placeholder='Common name'
-          name='name'
-          value={this.state.name}
-          onChange={e => this.handleChange(e)}
+        <div className="form-row">
+          <label>Common name</label>
+          <input
+            type='text'
+            placeholder='Common name'
+            name='name'
+            value={this.state.name}
+            onChange={e => this.handleChange(e)}
           />
-        <label>Scientific name</label>
-        <input
-          type='text'
-          placeholder='Scientific name'
-          name='scientific_name'
-          value={this.state.scientific_name}
-          onChange={e => this.handleChange(e)}
+        </div>
+        <div className="form-row">
+          <label>Scientific name</label>
+          <input
+            type='text'
+            placeholder='Scientific name'
+            name='scientific_name'
+            value={this.state.scientific_name}
+            onChange={e => this.handleChange(e)}
           />
-        <label>Region</label>
-        <input
-          type='text'
-          placeholder='Region'
-          name='region'
-          value={this.state.region}
-          onChange={e => this.handleChange(e)}
+        </div>
+        <div className="form-row">
+          <label>Region</label>
+          <input
+            type='text'
+            placeholder='Region'
+            name='region'
+            value={this.state.region}
+            onChange={e => this.handleChange(e)}
           />
-        <label>Average height</label>
-        <input
-          type='text'
-          placeholder='Average height'
-          name='average_height'
-          value={this.state.average_height}
-          onChange={e => this.handleChange(e)}
+        </div>
+        <div className="form-row">
+          <label>Average height</label>
+          <input
+            type='text'
+            placeholder='Average height'
+            name='average_height'
+            value={this.state.average_height}
+            onChange={e => this.handleChange(e)}
           />
-        <label>Lifespan</label>
-        <input
-          type='text'
-          placeholder='Lifespan'
-          name='lifespan'
-          value={this.state.lifespan}
-          onChange={e => this.handleChange(e)}
+        </div>
+        <div className="form-row">
+          <label>Lifespan</label>
+          <input
+            type='text'
+            placeholder='Lifespan'
+            name='lifespan'
+            value={this.state.lifespan}
+            onChange={e => this.handleChange(e)}
           />
-        <label>Fun fact</label>
-        <input
-          type='text'
-          placeholder='Fun fact'
-          name='fact'
-          value={this.state.fact}
-          onChange={e => this.handleChange(e)}
+        </div>
+        <div className="form-row">
+          <label>Fun fact</label>
+          <input
+            type='text'
+            placeholder='Fun fact'
+            name='fact'
+            value={this.state.fact}
+            onChange={e => this.handleChange(e)}
           />
-        <label>Image URL</label>
-        <input
-          type='text'
-          placeholder='Image URL'
-          name='image'
-          value={this.state.image}
-          onChange={e => this.handleChange(e)}
+        </div>
+        <div className="form-row">
+          <label>Image URL</label>
+          <input
+              type='text'
+              placeholder='Image URL'
+              name='image'
+              value={this.state.image}
+              onChange={e => this.handleChange(e)}
           />
+        </div>
         <button className="submit-tree" onClick={e => this.submitTree(e)}>Add Tree</button>
       </form>
     )
